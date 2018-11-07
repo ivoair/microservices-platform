feaabbf714c5bd1b2fef8d8316b2bd055c04ce7a
Iván Carro Rojo <ivancarro@gmail.com>
2018-11-07 17:44:04 +0100

    Boot Admin Server: initial commit

--- adminserver/src/main/java/com/icarrolab/admin/server/AdminServerApplication.java
+++ adminserver/src/main/java/com/icarrolab/admin/server/AdminServerApplication.java
@@ -1,16 +1,17 @@
-package com.icarrolab.config.configserver;
+package com.icarrolab.admin.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+
+@EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
-public class ConfigserverApplication {
+public class AdminServerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ConfigserverApplication.class, args);
+		SpringApplication.run(AdminServerApplication.class, args);
 	}
 }
